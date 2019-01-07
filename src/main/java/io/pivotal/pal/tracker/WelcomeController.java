c1671bafe7671dbd72e784ef597f94e541877bf1
suhasraok <inbox.ashishagrawal@yahoo.com>
2019-01-07 16:55:20 +0530

    Externalized properties

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+
+    private String message;
+
+
+    public WelcomeController( @Value("${WELCOME_MESSAGE}") String message) {
+        this.message = message;
+    }
+
+
     @GetMapping("/")
     public ResponseEntity<String> sayHello() {
-        return new ResponseEntity<>("hello", HttpStatus.OK);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
